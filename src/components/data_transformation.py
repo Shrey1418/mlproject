3b8958d5124145ba83e3122671d81ee5e9a33808
Shrey1418 <shreyaggrawal18@gmail.com>
2026-09-01 14:02:17 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -10,6 +10,7 @@ from src.exception import CustomException
 from src.logger import logging
 import os
 from src.utils import save_object
+from src.components.data_ingestion import DataIngestion
 
 
 @dataclass
