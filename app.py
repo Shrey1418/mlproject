2986faa4d2b29dc808ee5c1260b048d22e24181c
Shrey1418 <shreyaggrawal18@gmail.com>
2026-09-02 11:23:25 +0530

    Modified App file

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from flask import Flask,request,render_template
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+from flask import Flask, request, render_template
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 
